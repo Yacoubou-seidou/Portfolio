616d08e6caf4cd301ca9102146fbb7c5a5bbf5ee
yacoubou-seidou <yacoseidou@gmail.com>
2023-03-09 08:42:50 +0100

    Add data storage from form element

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const workSection = document.getElementById('work');
 const message = document.getElementById('validate_el');
 const mail = document.getElementById('email');
 const formSubmit = document.querySelector('.form_submit');
+const inputEmail = document.getElementById('email');
+const inputName = document.getElementById('name');
+const inputTextArea = document.getElementById('message');
 let btnModal;
 let span;
 const projects = [
@@ -170,7 +173,7 @@ function displayCard() {
     });
   });
 }
-displayCard();
+
 hamburger.addEventListener('click', toggleMenu);
 menuItems.forEach((menuItem) => {
   menuItem.addEventListener('click', toggleMenu);
@@ -181,3 +184,24 @@ formSubmit.addEventListener('submit', (event) => {
     event.preventDefault();
   }
 });
+
+formSubmit.addEventListener('input', () => {
+  const formData = {
+    name: inputName.value,
+    email: inputEmail.value,
+    message: inputTextArea.value,
+  };
+
+  localStorage.setItem('contactForm', JSON.stringify(formData));
+});
+
+function loadData() {
+  const formObj = JSON.parse(localStorage.getItem('contactForm'));
+  if (formObj) {
+    inputName.value = formObj.name;
+    inputEmail.value = formObj.email;
+    inputTextArea.value = formObj.message;
+  }
+}
+displayCard();
+loadData();
